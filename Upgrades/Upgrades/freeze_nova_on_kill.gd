c4baa77e2dc066e6df66cb500fa0df9a8d50ef27
Juiin <dadude000@gmail.com>
2026-02-10 17:03:48 +0100

    shatter + freez nova cost reduced; margin added to upgrade entry

--- Upgrades/Upgrades/freeze_nova_on_kill.gd
+++ Upgrades/Upgrades/freeze_nova_on_kill.gd
@@ -6,7 +6,7 @@ extends Upgrade
 func _init(_req_upgrade: Upgrade):
     upgrade_name = "[url][color=#7fd4ff]Freeze[/color][/url] Nova on Kill"
     upgrade_description = "10% chance to erupt a [url][color=#7fd4ff]freezing[/color][/url] Nova when killing a [url][color=#7fd4ff]frozen[/color][/url] enemy"
-    cost = [100, 250, 350, 500, 750, 1000, 1250, 1500, 2000, 2500]
+    cost = [100, 200, 300, 450, 650, 1000, 1250, 1500, 2000, 2500]
     type = Enums.UpgradeType.ICE_SPEAR
     has_freeze_hint = true
     #req_upgrade = _req_upgrade
--- Upgrades/Upgrades/shatter_on_kill.gd
+++ Upgrades/Upgrades/shatter_on_kill.gd
@@ -6,7 +6,7 @@ extends Upgrade
 func _init():
     upgrade_name = "Shatter on Kill"
     upgrade_description = "10% chance for [url][color=#7fd4ff]frozen[/color][/url] enemies to shatter into 5 ice shards on kill, dealing 5 damage each"
-    cost = [100, 150, 250, 400, 600, 800, 1000, 1250, 1500, 1750]
+    cost = [100, 150, 250, 350, 500, 700, 1000, 1250, 1500, 1750]
     type = Enums.UpgradeType.ICE_SPEAR
     #req_upgrade = _req_upgrade
     has_freeze_hint = true
